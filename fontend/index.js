5794b7ca294e5c88e5e1a4e74d4127f46c0ee502
Phạm Văn Hoàng <iloveanh18@gmail.com>
2023-11-28 04:17:08 +0700

    update homepage (desktop/mobile)

---
+++ fontend/index.js
@@ -0,0 +1,15 @@
+// search pop up
+const seacrhIcon = document.querySelector("#search-icon");
+const searchPopup = document.querySelector(".search_popup");
+seacrhIcon.addEventListener("click", () => {
+    searchPopup.classList.add("active");
+    document.body.style.overflow = "hidden";
+});
+if (searchPopup) {
+    searchPopup.addEventListener("click", (e) => {
+        if (e.target.classList.contains("search_popup")) {
+            searchPopup.classList.remove("active");
+            document.body.style.overflow = "auto";
+        }
+    });
+}
