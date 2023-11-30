c4e69c498f5e13e7897469da4732bba1d792db5f
Phạm Văn Hoàng <iloveanh18@gmail.com>
2023-11-30 20:14:34 +0700

    update title page

--- view/header.php
+++ view/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BeePoly</title>
     <link rel="stylesheet" href="<?php echo $url?>view/css/style.css">
     <link rel="stylesheet" href="<?php echo $url?>view/css/home.css">
     <link rel="stylesheet" href="<?php echo $url?>view/css/blog.css">
